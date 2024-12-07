5abde01d27b687d321eac21b72d98ab205b0a509
MikaelVroom <89934743+MikaelVroom@users.noreply.github.com>
2024-12-07 16:16:16 -0600

    Add files via upload
    Added CAN receiving

--- transmitter-test-code.py
+++ transmitter-test-code.py
@@ -1,38 +1,58 @@
 import os
 import board
 import digitalio
+import busio
 import time
 import espnow
 import wifi
+from canio import CAN as CAN1
+from adafruit_mcp2515 import MCP2515 as CAN2
+from canio import Message, RemoteTransmissionRequest
 
-print(f"My MAC address: {[hex(i) for i in wifi.radio.mac_address]}")
+# print(f"My MAC address: {[hex(i) for i in wifi.radio.mac_address]}")
 
 print(f"Connecting to {os.getenv('CIRCUITPY_WIFI_SSID')}")
 wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD"))
 print(f"Connected to {os.getenv('CIRCUITPY_WIFI_SSID')}")
 print(f"My IP address: {wifi.radio.ipv4_address}")
 
+# Setup CAN1 via canio
+can1 = CAN1(rx=board.IO6, tx=board.IO7, baudrate=500_000, auto_restart=True)
+
 e = espnow.ESPNow()
 peer = espnow.Peer(mac=b'\x74\x4D\xBD\x9D\x4D\x14')
 e.peers.append(peer)
 
 while True:
-#e.send("Starting...")
-    for i in range(100):
-        x = str(i)
-        e.send(x)
-        print("sending: " + x)
-        time.sleep(0.05)
-    time.sleep(0.5)
-    print("Time to count down")
+    print("CAN1: Tx Errors:", can1.transmit_error_count,
+    "Rx Errors:", can1.receive_error_count,
+    "state:", can1.state)
+    with can1.listen(timeout=1.0) as can1_listener:
+        message_count = can1_listener.in_waiting()
+        if message_count:
+            print("CAN1: Messages available:", message_count)
+            for _i in range(message_count):
+                msg = can1_listener.receive()
+                if isinstance(msg, Message):
+                    if hex(msg.id) == "0x3e8":
+                        print("CAN1: Recieved", msg.data, "from", hex(msg.id))
+                if isinstance(msg, RemoteTransmissionRequest):
+                    print("CAN1: RTR request length", msg.length, "from", hex(msg.id))
+    time.sleep(0.1)
+#    for i in range(101):
+#        x = str(i)
+#        e.send(x)
+#        print("sending: " + x)
+#        time.sleep(0.1)
+#    time.sleep(0.5)
+#    print("Time to count down")
 
-    for i in range(100):
-        x = str(100 - i)
-        e.send(x)
-        print("sending: " + x)
-        time.sleep(0.05)
-    time.sleep(0.5)    
-#e.send(b'end')
+#    for i in range(100):
+#        x = str(100 - i)
+#        e.send(x)
+#        print("sending: " + x)
+#        time.sleep(0.1)
+#    time.sleep(0.5)    
 
 print("finished sending")
 # For the ESP32-CAN-X2 the LED is on IO2
@@ -44,5 +64,3 @@ while True:
     time.sleep(0.5)
     led.value = False
     time.sleep(0.5)
-
-
