12f44668c34e970eabe0d4eb38d510a5e99c366b
MikaelVroom <89934743+MikaelVroom@users.noreply.github.com>
2024-12-14 17:38:40 -0600

    Update gauge3-code.py
    Reading all values with full ESP packet

--- gauge3-code.py
+++ gauge3-code.py
@@ -380,9 +380,54 @@ print(f"My IP address: {wifi.radio.ipv4_address}")
 e = espnow.ESPNow()
 packets = []
 
-mpgavg_last = 0
-ign_last = 0
-buff_cnt = 0
+map_kpa = 0
+map_tar = 0
+boost_state = 0
+flex_perc = 0
+fan_perc = 0
+
+iat = 0
+ect = 0
+mpg_avg = 0
+mpg_avg_last = 0
+mpg_inst = 0
+
+lambda1 = 0
+lambda2 = 0
+lambda_tar = 0
+
+fuel_kpa = 0
+fuel_tar = 0
+fuel_pwm = 0
+oil_kpa = 0
+
+knock_1 = 0
+knock_2 = 0
+knock_3 = 0
+knock_4 = 0
+knock_5 = 0
+knock_6 = 0
+
+ign_adv = 0
+ign_adv_last = 0
+tps = 0
+idle_state = 0
+
+vvt_int_tar = 0
+vvt_exh_tar = 0
+vvt_b1_int = 0
+vvt_b1_exh = 0
+vvt_b2_int = 0
+vvt_b2_ext = 0
+
+vss_lf = 0
+vss_rf = 0
+vss_lr = 0
+vss_rr = 0
+vss_driven = 0
+batt_v = 0
+fault_count = 0
+
 
 print("starting listen")
 
@@ -392,35 +437,69 @@ while True:
         packet = e.read()
 #        packets.append(packet)
         print(packet.msg)
-        if packet.msg == b'end':
-            break
         contents = packet.msg
-        packet_decoded = contents.decode('utf-8')
-#        print("Decoded: " + packet_decoded)
-        packet_float = float(packet_decoded)
-#        print("Str: " + str(packet_float))
-        packet_int = int(packet_float)
-        print("Int: " + str(packet_int))
-        mpgavg = round((packet_int / 10.1) + 15, 1)
-        mpginst = (packet_int / 2.5)
-        ign = int((packet_int / 2.51) - 10)
-        flex = packet_int
-        k1 = round((packet_int / 25) - 4, 1)
-        k2 = round((packet_int / 25.1) - 4, 1)
-        k3 = round((packet_int / 25.2) - 4, 1)
-        k4 = round((packet_int / 25.3) - 4, 1)
-        k5 = round((packet_int / 25.4) - 4, 1)
-        k6 = round((packet_int / 25.5) - 4, 1)
-        knock_levels = [k1,k2,k3,k4,k5,k6]
+
+        batt_v = ((contents[40] * 256 + (contents[41]))/100)
+        print(f"5: {contents[5]}")
+        print(f"Batt V: {batt_v}")
+  
+        map_kpa = round(((contents[0] * 256) + contents[1])/100,0)
+        map_tar = round(((contents[2] * 256) + contents[3])/100,0)
+        boost_state = contents[4]
+        flex_perc = contents[5]
+        fan_perc = contents[6]
+                        
+        iat = round(((contents[8] * 256) + contents[9])/100,0)
+        ect = round(((contents[10] * 256) + contents[11])/100,0)
+        mpg_avg = round(((contents[12] * 256) + contents[13])/100,0)
+        mpg_inst = contents[14]
+
+
+        lambda1 = round(((contents[15] * 256) + contents[16])/1000,3)
+        lambda2 = round(((contents[17] * 256) + contents[18])/1000,3)
+        lambda_tar = round(((contents[19] * 256) + contents[20])/1000,3)
+        fault_count = contents[21]
+
+        fuel_kpa = round(((contents[22] * 256) + contents[23])/100,0)
+        fuel_tar = round(((contents[24] * 256) + contents[25])/100,0)
+        fuel_pwm = contents[26]
+        oil_kpa = round(((contents[27] * 256) + contents[28])/100,0)
+
+        knock_1 = round((contents[29])/10,1)
+        knock_2 = round((contents[30])/10,1)
+        knock_3 = round((contents[31])/10,1)
+        knock_4 = round((contents[32])/10,1)
+        knock_5 = round((contents[33])/10,1)
+        knock_6 = round((contents[34])/10,1)
+
+        ign_adv = round(((contents[33] * 256) + contents[34])/100,0)
+        tps = round(((contents[35] * 256) + contents[36])/100,1)
+        idle_state = contents[37]
+        tps = round(((contents[38] * 256) + contents[39])/100,1)
+
+        vvt_int_tar = round((contents[40])/3,0)
+        vvt_exh_tar = round((contents[41])/3,0)
+        vvt_b1_int = round((contents[42])/3,1)
+        vvt_b1_exh = round((contents[43])/3,1)
+        vvt_b2_int = round((contents[44])/3,1)
+        vvt_b2_ext = round((contents[45])/3,1)
+
+        vss_lf = (contents[1])
+        vss_rf = (contents[2])
+        vss_lr = (contents[3])
+        vss_rr = (contents[4])
+        vss_driven = round(((contents[5] * 256) + contents[6])/100,1)
+
+        knock_levels = [knock_1,knock_2,knock_3,knock_4,knock_5,knock_6]
         max_knock = max(knock_levels)
-        if mpgavg != mpgavg_last:
+        if mpg_avg != mpg_avg_last:
             value_h.text = str(mpgavg)
-            mpgavg_last = mpgavg
-        if ign != ign_last:
-            value_j.text = str(ign)
-            ign_last = ign
-        value_k.text = str(flex)
-        gauge_i_bkgd.height = int((mpginst/40) * 480)
+            mpg_avg_last = mpg_avg
+        if ign_adv != ign_adv_last:
+            value_j.text = str(ign_adv)
+            ign_adv_last = ign_adv
+        value_k.text = str(flex_perc)
+        gauge_i_bkgd.height = int((mpg_inst/40) * 480)
         gauge_i_bkgd.y = int(480 - gauge_i_bkgd.height)
         value_k1.text = str(max_knock)
 
@@ -428,7 +507,7 @@ while True:
             knock_block.color_index = 0
         if max_knock > value_k1_min:
             knock_block.color_index = 1
-            
+
 print("packets:", f"length={len(packets)}")
 for packet in packets:
     print(packet)
