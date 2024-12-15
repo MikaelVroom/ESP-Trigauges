90090f2e90c2fa87ec12c43506f3ad471628fccc
MikaelVroom <89934743+MikaelVroom@users.noreply.github.com>
2024-12-15 10:58:20 -0600

    Update gauge3-code.py
    fixed addressing

--- gauge3-code.py
+++ gauge3-code.py
@@ -33,7 +33,9 @@ io = IO_HTTP(aio_username, aio_key, requests)
 
 graphics = Graphics(Displays.ROUND21, default_bg=None, auto_refresh=True)
 
-main_group = displayio.Group()
+gauge_1 = displayio.Group()
+gauge_2 = displayio.Group()
+gauge_3 = displayio.Group()
 
 font = bitmap_font.load_font("fonts/LeagueSpartan-Bold-16.bdf", Bitmap)
 
@@ -42,13 +44,13 @@ def center(grid, bitmap):
     grid.x -= graphics.display.width // 2
     grid.y -= graphics.display.height // 2
 
-graphics.display.root_group = main_group
+graphics.display.root_group = gauge_3
 
 color_bitmap = displayio.Bitmap(480, 480, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = 0x000000
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-main_group.append(bg_sprite)
+#main_group.append(bg_sprite)
 
 text = str("ini")
 
@@ -346,28 +348,28 @@ value_k1 = label.Label(
     anchored_position=(360, 320)
 )
 
-main_group.append(knock_block)
-main_group.append(lower_divider)
-main_group.append(channel_h)
-main_group.append(unit_h)
-main_group.append(value_h)
-main_group.append(gauge_i_frame)
-main_group.append(gauge_i_bkgd)
-main_group.append(channel_j)
-main_group.append(unit_j)
-main_group.append(value_j)
-main_group.append(channel_k)
-main_group.append(unit_k)
-main_group.append(value_k)
-main_group.append(div_1)
-main_group.append(div_2)
-main_group.append(div_3)
-main_group.append(div_4)
-main_group.append(mpg_scale_1)
-main_group.append(mpg_scale_2)
-main_group.append(mpg_scale_3)
-main_group.append(channel_l)
-main_group.append(value_k1)
+gauge_3.append(knock_block)
+gauge_3.append(lower_divider)
+gauge_3.append(channel_h)
+gauge_3.append(unit_h)
+gauge_3.append(value_h)
+gauge_3.append(gauge_i_frame)
+gauge_3.append(gauge_i_bkgd)
+gauge_3.append(channel_j)
+gauge_3.append(unit_j)
+gauge_3.append(value_j)
+gauge_3.append(channel_k)
+gauge_3.append(unit_k)
+gauge_3.append(value_k)
+gauge_3.append(div_1)
+gauge_3.append(div_2)
+gauge_3.append(div_3)
+gauge_3.append(div_4)
+gauge_3.append(mpg_scale_1)
+gauge_3.append(mpg_scale_2)
+gauge_3.append(mpg_scale_3)
+gauge_3.append(channel_l)
+gauge_3.append(value_k1)
 
 
 # print(f"My MAC address: {[hex(i) for i in wifi.radio.mac_address]}")
@@ -389,7 +391,7 @@ fan_perc = 0
 iat = 0
 ect = 0
 mpg_avg = 0
-mpg_avg_last = 0
+mpg_avg_last = 1
 mpg_inst = 0
 
 lambda1 = 0
@@ -409,7 +411,7 @@ knock_5 = 0
 knock_6 = 0
 
 ign_adv = 0
-ign_adv_last = 0
+ign_adv_last = 1
 tps = 0
 idle_state = 0
 
@@ -440,7 +442,7 @@ while True:
         contents = packet.msg
 
         batt_v = ((contents[40] * 256 + (contents[41]))/100)
-        print(f"5: {contents[5]}")
+        print(f"55: {contents[55]}")
         print(f"Batt V: {batt_v}")
   
         map_kpa = round(((contents[0] * 256) + contents[1])/100,0)
@@ -449,52 +451,54 @@ while True:
         flex_perc = contents[5]
         fan_perc = contents[6]
                         
-        iat = round(((contents[8] * 256) + contents[9])/100,0)
-        ect = round(((contents[10] * 256) + contents[11])/100,0)
-        mpg_avg = round(((contents[12] * 256) + contents[13])/100,0)
-        mpg_inst = contents[14]
+        iat = round(((contents[7] * 256) + contents[8])/100,0)
+        ect = round(((contents[9] * 256) + contents[10])/100,0)
+        mpg_avg = round(((contents[11] * 256) + contents[12])/100,1)
+        mpg_inst = contents[13]
 
 
-        lambda1 = round(((contents[15] * 256) + contents[16])/1000,3)
-        lambda2 = round(((contents[17] * 256) + contents[18])/1000,3)
-        lambda_tar = round(((contents[19] * 256) + contents[20])/1000,3)
-        fault_count = contents[21]
+        lambda1 = round(((contents[14] * 256) + contents[15])/1000,3)
+        lambda2 = round(((contents[16] * 256) + contents[17])/1000,3)
+        lambda_tar = round(((contents[18] * 256) + contents[19])/1000,3)
+        fault_count = contents[20]
 
-        fuel_kpa = round(((contents[22] * 256) + contents[23])/100,0)
-        fuel_tar = round(((contents[24] * 256) + contents[25])/100,0)
-        fuel_pwm = contents[26]
-        oil_kpa = round(((contents[27] * 256) + contents[28])/100,0)
+        fuel_kpa = round(((contents[21] * 256) + contents[22])/100,0)
+        fuel_tar = round(((contents[23] * 256) + contents[24])/100,0)
+        fuel_pwm = contents[25]
+        oil_kpa = round(((contents[26] * 256) + contents[27])/100,0)
 
-        knock_1 = round((contents[29])/10,1)
-        knock_2 = round((contents[30])/10,1)
-        knock_3 = round((contents[31])/10,1)
-        knock_4 = round((contents[32])/10,1)
-        knock_5 = round((contents[33])/10,1)
-        knock_6 = round((contents[34])/10,1)
+        knock_1 = round((contents[28])/10,1)
+        knock_2 = round((contents[29])/10,1)
+        knock_3 = round((contents[30])/10,1)
+        knock_4 = round((contents[31])/10,1)
+        knock_5 = round((contents[32])/10,1)
+        knock_6 = round((contents[33])/10,1)
 
-        ign_adv = round(((contents[33] * 256) + contents[34])/100,0)
-        tps = round(((contents[35] * 256) + contents[36])/100,1)
-        idle_state = contents[37]
-        tps = round(((contents[38] * 256) + contents[39])/100,1)
+        ign_adv = round(((contents[35] * 256) + contents[36])/100,0)
+        tps = round(((contents[37] * 256) + contents[38])/100,1)
+        idle_state = contents[39]
+        tps = round(((contents[40] * 256) + contents[41])/100,1)
 
-        vvt_int_tar = round((contents[40])/3,0)
-        vvt_exh_tar = round((contents[41])/3,0)
-        vvt_b1_int = round((contents[42])/3,1)
-        vvt_b1_exh = round((contents[43])/3,1)
-        vvt_b2_int = round((contents[44])/3,1)
-        vvt_b2_ext = round((contents[45])/3,1)
+        vvt_int_tar = round((contents[42])/3,0)
+        vvt_exh_tar = round((contents[43])/3,0)
+        vvt_b1_int = round((contents[44])/3,1)
+        vvt_b1_exh = round((contents[45])/3,1)
+        vvt_b2_int = round((contents[46])/3,1)
+        vvt_b2_ext = round((contents[47])/3,1)
 
-        vss_lf = (contents[1])
-        vss_rf = (contents[2])
-        vss_lr = (contents[3])
-        vss_rr = (contents[4])
-        vss_driven = round(((contents[5] * 256) + contents[6])/100,1)
+        vss_lf = (contents[49])
+        vss_rf = (contents[50])
+        vss_lr = (contents[51])
+        vss_rr = (contents[52])
+        vss_driven = round(((contents[53] * 256) + contents[54])/100,1)
+
+        screen_number = contents[55]
 
         knock_levels = [knock_1,knock_2,knock_3,knock_4,knock_5,knock_6]
         max_knock = max(knock_levels)
-        if mpg_avg != mpg_avg_last:
-            value_h.text = str(mpgavg)
-            mpg_avg_last = mpg_avg
+#        if mpg_avg != mpg_avg_last:
+        value_h.text = str(mpg_avg)
+#            mpg_avg_last = mpg_avg
         if ign_adv != ign_adv_last:
             value_j.text = str(ign_adv)
             ign_adv_last = ign_adv
